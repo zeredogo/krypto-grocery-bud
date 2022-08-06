29e8b95d3459fec02d4c0c4004c63f9119783725
zeredogo <zeredogo@gmail.com>
2022-08-07 00:01:53 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import List from './List';
 import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const[isEditing, setIsEditing] = useState(false);
+  const[editID, setEditID] = useState(null);
+  const[alert, setAlert] = useState({ show: false, msg: '',
+  type: ''}); 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
+  return <section className='section-center'>
+    <div className='grocery-container'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input type='text' className='grocery' placeholder=''/>
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit '}
+          </button>
+        </div>
+      </form>
+      <List />
+      <button className='clear-btn'>clear items</button>
+    </div>
+  </section>
 }
 
 export default App
